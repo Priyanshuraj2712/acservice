153f1b92c5151e74662a86f15e87c68dc00bab9d
Pawan Sharma <68536005+pawansharma111104@users.noreply.github.com>
2025-01-23 19:58:10 +0530

    msg

--- ac-service/pages/_document.js
+++ ac-service/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/logo.png" />
+        {/* Add additional link tags for other favicon files and sizes here */}
+      </Head>
       <body>
         <Main />
         <NextScript />
