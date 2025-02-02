a7a0717e43828a7630bfd1e5bfce5e1b066d86ac
Priyanshuraj2712 <priyanshu22csu136@ncuindia.edu>
2025-02-02 21:13:35 +0530

    main

--- ac-service/Components/Footer.js
+++ ac-service/Components/Footer.js
@@ -1,35 +1,34 @@
 import React from "react";
+import Link from "next/link";
 import { FaFacebookF, FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import styles from "@/styles/Footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      {/* Wave Animation */}
       <div className={styles.wave}></div>
 
       <div className={styles.container}>
-        {/* Footer Links Section */}
         <div className={styles.footerLinks}>
           <div>
             <h4>Company</h4>
             <ul>
-              <li>About Us</li>
-              <li>Services</li>
-              <li>Why Choose Us?</li>
-              <li>Blog</li>
-              <li>Contact Us</li>
+              <li><Link href="/about">About Us</Link></li>
+              <li><Link href="/">Services</Link></li>
+              <li><Link href="/why-choose-us">Why Choose Us?</Link></li>
+              <li><Link href="/blog">Blog</Link></li>
+              <li><Link href="/contact">Contact</Link></li>
             </ul>
           </div>
 
           <div>
             <h4>Support</h4>
             <ul>
-              <li>FAQs</li>
-              <li>Help Center</li>
-              <li>Terms & Conditions</li>
-              <li>Privacy Policy</li>
-            </ul>
+              <li><Link href="/">FAQs</Link></li>  
+              <li><Link href="/contact">Help Center</Link></li> 
+              <li><Link href="/">Terms & Conditions</Link></li> 
+              <li><Link href="/">Privacy Policy</Link></li>
+</ul>
           </div>
 
           <div>
