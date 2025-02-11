7e611a866cc3d2a45664e731df0496b6ac9fa0f6
Pawan Sharma <68536005+pawansharma111104@users.noreply.github.com>
2025-02-11 22:54:04 +0530

    Deploy Next.js to GitHub Pages

--- ac-service/next.config.mjs
+++ ac-service/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Enables static export
-  basePath: "/acservice", // Replace with your GitHub repo name
+  basePath: "/ac-service", // Replace with your GitHub repo name
   images: {
     unoptimized: true, // Fixes issues with Next.js Image component
   },
