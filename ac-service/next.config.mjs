c1e695ef8dc1043b002a7390656c3afbc10d437d
Pawan Sharma <68536005+pawansharma111104@users.noreply.github.com>
2025-02-13 17:41:52 +0530

    msg

--- ac-service/next.config.mjs
+++ ac-service/next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Enables static export
-  basePath: "/ac-service", // Replace with your GitHub repo name
-  images: {
-    unoptimized: true, // Fixes issues with Next.js Image component
-  },
+  reactStrictMode: true,
+  trailingSlash: false, // Default Next.js behavior
 };
 
 export default nextConfig;
