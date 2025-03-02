7805ecb52eb6164456aca2a1677c0c6632abc4ab
Pawan Sharma <68536005+pawansharma111104@users.noreply.github.com>
2025-03-02 19:48:57 +0530

    msg

--- ac-service/pages/_app.js
+++ ac-service/pages/_app.js
@@ -1,5 +1,39 @@
+import { useEffect } from "react";
+import Script from "next/script";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "G-DVKLMJ80RV"); // Replace with your Measurement ID
+  }, []);
+
+  return (
+    <>
+      {/* Google Analytics Script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-DVKLMJ80RV`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DVKLMJ80RV', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
